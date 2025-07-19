76f8d14d160b29355cc7aa99b167d5933c37b658
ShayanIsrar <shayanisrar19@gmail.com>
2025-07-19 02:31:20 -0700

    updation
    updation

--- frontend/src/components/pages/AllProducts.jsx
+++ frontend/src/components/pages/AllProducts.jsx
@@ -8,12 +8,17 @@ const AllProducts = () => {
     <div>
       <div className="bg-white py-2 px-4 flex justify-between items-center">
         <h2 className="font-bold text-lg">All Products</h2>
-        <button className="border-2 border-red-600 text-red-600 hover:bg-red-600 hover:text-white transition-all py-1 px-3 rounded-full" onClick={()=>setOpenUploadProduct(true)}>
+        <button
+          className="border-2 border-red-600 text-red-600 hover:bg-red-600 hover:text-white transition-all py-1 px-3 rounded-full"
+          onClick={() => setOpenUploadProduct(true)}
+        >
           Upload Product
         </button>
       </div>
       {/* Upload Products component */}
-      {openUploadProduct && <UploadProduct on />}
+      {openUploadProduct && (
+        <UploadProduct onClose={() => setOpenUploadProduct(false)} />
+      )}
     </div>
   );
 };
